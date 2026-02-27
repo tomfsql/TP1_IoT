5b1ee86989ea71eef821a7e036e0a6a144089226
Tom Fasquelle <tom.fasquelle@etu.univ-lyon1.fr>
2026-02-27 10:50:33 +0100

    Ex 1 tp1 fonctionnel

---
+++ app/src/main/java/fr/cpe/fasquelle/helloworld/MainActivity.java
@@ -0,0 +1,76 @@
+package fr.cpe.fasquelle.helloworld;
+
+import static androidx.core.content.ContextCompat.getSystemService;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button bouton;
+    TextView display;
+    //String displayText;
+
+    //SensorManager mSensorManager;
+
+    //private SensorEventListener listener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        //mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            //displayText = display.getText().toString();
+            setContentView(R.layout.activity_main);
+            bouton = findViewById(R.id.bouton);
+            display = findViewById(R.id.button2);
+
+            bouton.setOnClickListener(
+                    view -> display.setText("Alors? c’est pas beau ça? :D")
+            );
+            return insets;
+        });
+    }
+}
+
+//        listener = new SensorEventListener() {
+//            @Override
+//            public void onSensorChanged(SensorEvent sensorEvent) {
+//                float light = sensorEvent.values[0];
+//            }
+//            @Override
+//            public void onAccuracyChanged(Sensor sensor, int i) {}
+//        };
+//    }
+
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        SensorManager manager = (SensorManager) getSystemService(SENSOR_SERVICE);
+//        Sensor sensor = manager.getDefaultSensor(Sensor.TYPE_LIGHT);
+//        if (sensor != null) {
+//            manager.registerListener(listener, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+//        }
+//    }
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        SensorManager manager = (SensorManager) getSystemService(SENSOR_SERVICE);
+//        manager.unregisterListener(listener);
+//    }
